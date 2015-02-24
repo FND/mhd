9c85ed35f274ff90159d2ba406d9d1a40ba8af85
FND <fnd@localhost.localdomain>
2015-02-24 08:18:38 +0100

    simplified internals

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from mhd.server import start_server
 def dispatch(req, res): # XXX: DEBUG; for demo purposes only
     res.status(200)
     res.header("Content-Length", "38")
-    info = " ".join((req.method, req.uri)).encode("ascii")
-    res.body(info + b"\n")
+    info = "%s %s\n" % (req.method, req.uri)
+    res.body(info.encode("ascii"))
     res.body(b"lorem ipsum\ndolor sit amet\n\n...\n")
 
 
